d0d92c42840b6d297e2e588daaff9c4bd1908019
Estela Extremera <estelaextremeracampos@gmail.com>
2023-11-08 13:38:11 +0100

    upload intro change

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -22,23 +22,16 @@ const Intro = () => {
             <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experiencia</h5>
-              <small>1 year</small>
+              <small>1 año</small>
             </article>
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
               <h5>Proyectos</h5>
-              <small>10+ Completed Projects</small>
+              <small>+16 Proyectos</small>
             </article>
           </div>
           <p>
-            1+ years experienced Front End Developer with hands-on experience in
-            identifying web-based user interactions along with designing and
-            implementing highly-responsive user interface components by
-            deploying React concepts. Proficient in translating designs and
-            wireframes into high-quality code, and writing application interface
-            code via JavaScript and React JS workflows. Adept at monitoring and
-            maintaining frontend performance and troubleshooting and debugging
-            the same to bolster overall performance.
+            Un año de experiencia como desarrolladora web Full Stack. Comencé con un bootcamp...
           </p>
           <a href="#contact" className="btn btn-primary">
             Let's Talk

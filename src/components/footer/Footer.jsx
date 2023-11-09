a2fd0faae7b3047ef904ea48a52a1ddb7cc1ea92
Estela Extremera <estelaextremeracampos@gmail.com>
2023-11-09 10:17:10 +0100

    change footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,24 +6,50 @@ import './footer.css';
 const Footer = () => {
   return (
     <footer>
-      <a href="#home" className="footer__logo">Rasif Tagizade</a>
+      <a href="#home" className="footer__logo">
+        Estela Extremera
+      </a>
       <ul className="permalinks">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Skills</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a href="#home">Página Principal</a>
+        </li>
+        <li>
+          <a href="#about">Sobre mi</a>
+        </li>
+        <li>
+          <a href="#experience">Skills</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonios</a>
+        </li>
+        <li>
+          <a href="#contact">Contacto</a>
+        </li>
       </ul>
       <div className="footer__socials">
-      <a href="https://www.linkedin.com/in/rasif-tagizada/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
-      <a href="https://github.com/Rasif-Taghizada" target="_blank" rel="noreferrer" ><FaGithub /></a>
+        <a
+          href="https://www.linkedin.com/in/estela-extremera"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://https://github.com/Estela-Ex"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
       </div>
       <div className="footer__copyright">
-        <small>&copy; ET 2022. All rights reserved.</small>
+        <small>&copy; ET 2023. Todos los derechos reservados.</small>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer
